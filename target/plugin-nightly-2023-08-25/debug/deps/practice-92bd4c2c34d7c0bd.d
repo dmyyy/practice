1e72ba66ae25984213f26b5d6932c463d0d96e56
dmyyy <dima_banin@hotmail.com>
2024-04-10 17:31:30 -0700

    add gitignore

--- target/plugin-nightly-2023-08-25/debug/deps/practice-92bd4c2c34d7c0bd.d
+++ target/plugin-nightly-2023-08-25/debug/deps/practice-92bd4c2c34d7c0bd.d
@@ -1,6 +1,6 @@
-/home/dmy/git/practice/target/plugin-nightly-2023-08-25/debug/deps/practice-92bd4c2c34d7c0bd.rmeta: src/lib.rs src/neetcode/mod.rs src/neetcode/arrays_hashing/mod.rs src/neetcode/arrays_hashing/contains_duplicate.rs src/neetcode/arrays_hashing/valid_anagram.rs src/neetcode/greedy/mod.rs src/neetcode/greedy/maximum_subarray.rs src/neetcode/two_pointers/mod.rs
+/home/dmy/git/practice/target/plugin-nightly-2023-08-25/debug/deps/practice-92bd4c2c34d7c0bd.rmeta: src/lib.rs src/neetcode/mod.rs src/neetcode/arrays_hashing/mod.rs src/neetcode/arrays_hashing/contains_duplicate.rs src/neetcode/arrays_hashing/valid_anagram.rs src/neetcode/greedy/mod.rs src/neetcode/greedy/maximum_subarray.rs src/neetcode/two_pointers/mod.rs src/neetcode/two_pointers/valid_palindrome.rs src/leetcode/mod.rs src/leetcode/add_two_numbers_445.rs
 
-/home/dmy/git/practice/target/plugin-nightly-2023-08-25/debug/deps/practice-92bd4c2c34d7c0bd.d: src/lib.rs src/neetcode/mod.rs src/neetcode/arrays_hashing/mod.rs src/neetcode/arrays_hashing/contains_duplicate.rs src/neetcode/arrays_hashing/valid_anagram.rs src/neetcode/greedy/mod.rs src/neetcode/greedy/maximum_subarray.rs src/neetcode/two_pointers/mod.rs
+/home/dmy/git/practice/target/plugin-nightly-2023-08-25/debug/deps/practice-92bd4c2c34d7c0bd.d: src/lib.rs src/neetcode/mod.rs src/neetcode/arrays_hashing/mod.rs src/neetcode/arrays_hashing/contains_duplicate.rs src/neetcode/arrays_hashing/valid_anagram.rs src/neetcode/greedy/mod.rs src/neetcode/greedy/maximum_subarray.rs src/neetcode/two_pointers/mod.rs src/neetcode/two_pointers/valid_palindrome.rs src/leetcode/mod.rs src/leetcode/add_two_numbers_445.rs
 
 src/lib.rs:
 src/neetcode/mod.rs:
@@ -10,3 +10,6 @@ src/neetcode/arrays_hashing/valid_anagram.rs:
 src/neetcode/greedy/mod.rs:
 src/neetcode/greedy/maximum_subarray.rs:
 src/neetcode/two_pointers/mod.rs:
+src/neetcode/two_pointers/valid_palindrome.rs:
+src/leetcode/mod.rs:
+src/leetcode/add_two_numbers_445.rs:
