6345115b0bac7c9eed27addd48b70d907f1bdfd9
dmyyy <dima_banin@hotmail.com>
2024-04-10 20:00:49 -0700

    valid_palindrome

--- src/neetcode/two_pointers/valid_palindrome.rs
+++ src/neetcode/two_pointers/valid_palindrome.rs
@@ -27,5 +27,37 @@ Since an empty string reads the same forward and backward, it is a palindrome.
 */
 
 pub fn is_palindrome(s: String) -> bool {
-    
+    // remove non-alphanumeric (letters/numbers)
+    let mut s: String = s.chars().filter(|c| c.is_alphanumeric()).map(|c| c.to_ascii_lowercase()).collect();
+    // convert to lower
+    s = s.to_lowercase();
+
+    if s.is_empty() {
+        return true
+    }
+    let mut curr = 0;
+    let mut end = s.len() - 1;    
+
+    let s_bytes = s.as_bytes();
+
+    while curr < end {
+        if s_bytes[curr] != s_bytes[end] {
+            return false
+        }
+        curr += 1;
+        end -= 1;
+    }
+    true
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_is_palindrome() {
+        assert_eq!(is_palindrome(String::from("")), true);
+        assert_eq!(is_palindrome(String::from(" ")), true);
+        assert_eq!(is_palindrome(String::from("A man, a plan, a canal: Panama")), true);
+        assert_eq!(is_palindrome(String::from("race a car")), false);
+    }
 }
